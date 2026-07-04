1c13bb7742102afc94d913f5e409eb1eb0663afc
thevindugimantha-blip <thevindugimantha@gmail.com>
2026-07-04 18:28:10 +0530

    Initial Spring Boot setup with MySQL configuration

---
+++ src/main/java/com/vehiclerental/VehicleRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vehiclerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleRentalSystemApplication.class, args);
+    }
+
+}
